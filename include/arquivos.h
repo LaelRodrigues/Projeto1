585e1b1a5817b1ad400bf9b36f4c57be0a87f485
LaelRodrigues <laelrodrigues7@gmail.com>
2017-05-01 22:03:48 -0300

    Atualização do arquivo arquivos.h

--- include/arquivos.h
+++ include/arquivos.h
@@ -1,11 +1,15 @@
 /**
  * @file	arquivos.h
- * @brief	implementacao de funcoes para realizam munipulacao 
+ * @brief	implementacao de funcoes para realizam munipulacoes
  *					em arquivos
  * @author	Lael Rodrigues(laelrodrigues7@gmail.com)
  * @since	30/04/2017
  * @date	01/05/2017
  */
+
+
+#include "estatisticas.h"
+#include "multmatrizes.h"
 
 #ifndef ARQUIVOS_H
 #define ARQUIVOS_H
@@ -24,6 +28,9 @@ using std::cout;
 #include <fstream>
 using std::ifstream;
 using std::ofstream;
+
+#include <chrono>
+using std::ratio;
 
 
 /**
@@ -71,12 +78,75 @@ void gerar_matriz_arquivo(ofstream &saida, T **matriz, int n) {
  * @param arquivo stream de entrada ou saida
  */
 template <typename T>
-void testaArquivo(T &arquivo, string nome) {
+void testaArquivo(T &arquivo) {
 	if(!arquivo) {
-		cout << "Erro ao abrir o arquivo " << nome << endl;
+		cout << "Erro ao abrir o arquivo " << endl;
 		cout << "Programa finalizado." << endl;
 		exit(1);
 	}
 }
 
+
+/**
+ * @brief Funcao para gerar estatisticas em relacao ao tempo
+ *				de execucao dos algoritmos recursivo e iterativo
+ * @param entrada Variavel que referencia o arquivo de entrada
+ * @param matriz Matriz que armazenara os dados lidos
+ * @param n Dimensoes da matriz
+ */
+template <typename T>
+void gerarEstatisticas(T **A, T**B, int tam) {
+
+	ofstream arqIterativo("../output/stats-ite.dat");
+	testaArquivo(arqIterativo);
+
+	float tempos[SEQUENCIA];
+	int **C;
+
+	for(int i = 0; i < SEQUENCIA; i++) {
+
+		auto tempo_ini = std::chrono::high_resolution_clock::now();
+		C = multiplicaI(A, B, tam);
+		auto tempo_fim = std::chrono::high_resolution_clock::now();
+		tempos[i] = std::chrono::duration<double, ratio<60>>(tempo_fim - tempo_ini).count();
+	}
+
+	float maior, menor, md, desv;
+	
+	maior = max(SEQUENCIA, tempos);
+	menor = max(SEQUENCIA, tempos);
+	md = media(SEQUENCIA, tempos);
+	desv = desv_padrao(SEQUENCIA, tempos);
+
+	arqIterativo << tam << " " << maior << " " << menor;
+	arqIterativo << md << " " << desv << endl;
+
+	arqIterativo.close();
+
+	ofstream arqRecursivo("../output/stats-rec.dat");
+	testaArquivo(arqRecursivo);
+
+	for(int i = 0; i < SEQUENCIA; i++) {
+		auto tempo_ini = std::chrono::high_resolution_clock::now();
+		C = multiplicaR(A, B, tam);
+		auto tempo_fim = std::chrono::high_resolution_clock::now();
+		tempos[i] = std::chrono::duration<double, ratio<60>>(tempo_fim - tempo_ini).count();
+	}
+
+	for(int i = 0; i < tam; i++) {
+		delete [] C[i];
+	}
+
+	delete [] C;
+
+	maior = max(SEQUENCIA, tempos);
+	menor = max(SEQUENCIA, tempos);
+	md = media(SEQUENCIA, tempos);
+	desv = desv_padrao(SEQUENCIA, tempos);
+
+	arqRecursivo << tam << " " << maior << " " << menor;
+	arqRecursivo << md << " " << desv << endl;
+
+	arqRecursivo.close();
+}
 #endif
